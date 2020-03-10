34052093437656b4409049d69b22e45b0475bf9d
Zellisback <zellisback@gmail.com>
2020-03-10 23:26:02 +0200

    Improve Block Breaker

---
+++ scripts/Block Breaker/GameStatus.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameStatus : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI scoreText;
+    [Range(0.1f , 10f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed =2;
+    [SerializeField] int currentScore = 0; //why grey
+
+    private void Awake() //singleton
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        CurrentScoreText();
+    }
+
+    private void CurrentScoreText()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+    void Update()
+    {
+        Time.timeScale = gameSpeed;  //timeScale metavlhth tou Time dn 8elei dhlwma?
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        CurrentScoreText();
+    }
+
+}
---
+++ scripts/Block Breaker/Level.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+
+    [SerializeField] int breakableBlocks;  // serialized for debuging
+    SceneLoader sceneLoader;
+
+    public void Start() // but why
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+    
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+
+}
